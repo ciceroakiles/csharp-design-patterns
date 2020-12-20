60a03c039feefd8b96a55b9a3497f5608224339c
ciceroakiles <cicero.akiles@gmail.com>
2020-12-20 14:18:27 -0300

    create

---
+++ src/Application/Decorators/DetranPontos.cs
@@ -0,0 +1,26 @@
+﻿using DesignPatternSamples.Application.DTO;
+using DesignPatternSamples.Application.Services;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DesignPatternSamples.Application.Decorators
+{
+    // Simulação de serviço: pontos na carteira, similar ao logger
+    public class DetranPontos : IDetranVerificadorDebitosService
+    {
+        private readonly IDetranVerificadorDebitosService _Inner;
+
+        public DetranPontos(IDetranVerificadorDebitosService inner)
+        {
+            _Inner = inner;
+        }
+
+        public async Task<IEnumerable<DebitoVeiculo>> ConsultarDebitos(Veiculo veiculo)
+        {
+            var result = await _Inner.ConsultarDebitos(veiculo);
+            Console.WriteLine(">>> Mensagem de teste: X pontos na carteira");
+            return result;
+        }
+    }
+}
